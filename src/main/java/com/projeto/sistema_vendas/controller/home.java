2f2b6ff940c96ecf3d39330c483def8b64857eff
Leo-s-santoz <leonardosantos11080@gmail.com>
2025-09-13 19:55:48 -0300

    primeiro commit

---
+++ src/main/java/com/projeto/sistema_vendas/controller/home.java
@@ -0,0 +1,13 @@
+package com.projeto.sistema_vendas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class home {
+
+    @GetMapping("/administrativo")
+    public String loadHome(){
+        return "administrativo/home";
+    }
+}
